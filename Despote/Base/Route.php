a195cd942ec6011b1bed7bc2b63786cd44f96b12
He110te4m <He110te4m@gmail.com>
2017-11-04 15:33:17 +0800

    优化路由类，增加传参功能

--- Despote/Base/Route.php
+++ Despote/Base/Route.php
@@ -14,17 +14,20 @@ class Route
         // 获取全局配置
         global $config;
 
-        // 获取 URL 中的 pathinfo
-        $pathInfo = !empty($_GET['r']) ? explode('/', $_GET['r']) : [];
-        // 获取控制器名称，如果不存在则使用默认值，默认值可以在 Config/Config.php 中修改
-        $className  = !empty($pathInfo[0]) ? $pathInfo[0] : $config['default']['controller'];
-        // 获取控制器方法，如果不存在则使用默认值，默认值可以在 Config/Config.php 中修改
-        $methodName = !empty($pathInfo[1]) ? $pathInfo[1] : $config['default']['methon'];
+        // 获取数据
+        $pathInfo   = !empty($_GET['r']) ? explode('/', $_GET['r']) : [];
+        $className  = array_shift($pathInfo);
+        $methodName = array_shift($pathInfo);
+
+        // 数据校验
+        is_null($className) && $className   = $config['default']['controller'];
+        is_null($methodName) && $methodName = $config['default']['methon'];
+
         // 拼接控制器
         $controller = 'App\Controller\\' . $className;
         // 实例化控制器
-        $obj        = new $controller();
+        $obj = new $controller();
         // 加载方法
-        $obj->$methodName();
+        call_user_func_array([$obj, $methodName], $pathInfo);
     }
 }
